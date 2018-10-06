34e1c9d1807abd6b02c7d5446af97235c5f52ca4
Mikko Litola <mikko.litola@condento.com>
2018-10-06 14:23:20 +0300

    Initial commit with a working version of the backend

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # TrivialLauncher
 Web based program launcher consisting of Vue.js frontend and Golang backend
+
+## Description
+Trivial Launcher is as the name implies a trivial software launcher. It consists of a frontend and a backend parts and it can be used as 
+a web based remote control for launching/stopping programs. I made this for my personal use, which will be launching 
+games/emulators/programs/etc. remotely in my living room pc. 
+
+## Frontend
+```
+TBA
+```
+
+## Backend
+
+### Requirements:
+
+- Go 10.0.0 (or newer)
+
+### Building a binary:
+```
+# set GOPATH i.e. here project in $HOME/go/src/TrivialLauncher
+export GOPATH=$HOME/go/src
+
+cd $HOME/go/src/TrivialLauncher/backend
+
+# this will compile a backend binary
+go build
+```
+
+### Running the binary:
+```
+./backend
+
+2018/10/06/ 14:09:01 Starting Trivial Launcher backend at localhost:8080
+```
+
+### Configuration and launching/stopping programs
+The programs you want to launch can be configured into the config.json file. The config.json can be requested with a GET operation to 
+an endpoint /all and i.e. 
+```
+wget -qO- http://localhost:8080/all
+{"program":[{"name":"Madplay","endpoint":"/madplay","file":"madplay","args":"/home/user/Music/song.mp3"}
+```
+A program entity consists of four parameter: name, endpoint, file and args. Let's say I want to launch a madplay and play some .mp3 file 
+then I can configure parameters: { "name" : "Madplay", "endpoint" : "/madplay", "file" : "madplay", "args" "/home/user/Music/song.mp3" }. 
+To test this and launch do a GET operation for the endpoint i.e.
+```
+wget -qO- http://localhost:8080/madplay &> /dev/null
+```
+For stopping an ongoing program execution we can do a GET operation to an endpoint /endCurrent i.e.
+```
+wget -qO- http://localhost:8080/endCurrent &> /dev/null
+
+```
---
+++ backend/backend.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+type Programs struct {
+	Programs []Program `json:"program"`
+}
+
+type Program struct {
+	Name 		string `json:"name,omitempty"`
+	Endpoint  	string `json:"endpoint,omitempty"`
+	File   		string `json:"file,omitempty"`
+	Args   		string `json:"args,omitempty"`
+}
+
+var configFile string = "./config.json"
+var port = "8080"
+
+var programs Programs
+
+// open configFile and populate program list
+func parseConfigFile() {
+	file, err := os.Open(configFile)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	bytes, _ := ioutil.ReadAll(file)
+
+	json.Unmarshal(bytes, &programs)
+}
+
+// run the initialized command
+func runCommand(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	cmd.Start()
+}
+
+// kill a running command process
+func killCommand(cmd *exec.Cmd) {
+	pgid, err := syscall.Getpgid(cmd.Process.Pid)
+	if err == nil {
+    		syscall.Kill(-pgid, 15) //weirdness of golang -> pgid is negative...
+	}
+}
+
+func main() {
+	parseConfigFile()
+
+	var currentCommand *exec.Cmd
+	var currentRunningProcess string
+
+	// get all endpoint that gets the whole config json
+	http.HandleFunc("/all", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(programs)
+	})
+
+	// create other endpoints from the program array
+	for i := 0; i < len(programs.Programs); i++ {
+		bufName := programs.Programs[i].Name
+		bufEndpoint := programs.Programs[i].Endpoint
+		bufFile := programs.Programs[i].File
+		bufArgs := programs.Programs[i].Args
+
+		http.HandleFunc(bufEndpoint, func(w http.ResponseWriter, r *http.Request) {
+			currentCommand = exec.Command(bufFile, bufArgs) // TODO: add support for more args
+			currentRunningProcess = bufName
+			log.Printf("Launching %s from an endpoint: %s\n", bufName, bufEndpoint)
+			go runCommand(currentCommand)
+		})
+	}
+
+	// end current command execution if one exists
+	http.HandleFunc("/endCurrent", func(w http.ResponseWriter, r *http.Request) {
+		if currentCommand != nil {
+			log.Println("Ending current process: " + currentRunningProcess)
+			go killCommand(currentCommand)
+		}
+	})
+
+	log.Printf("Starting Trivial Launcher backend at localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(":" + port, nil))
+}
